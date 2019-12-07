5e82b002561a5f9a186f3ff0e807570fe62a274d
stefaleon <stefaleon@gmail.com>
2019-12-07 15:25:22 +0200

    code reuse with beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,20 +5,26 @@ import App from "../../App";
 import CommentBox from "../CommentBox";
 import CommentList from "../CommentList";
 
-it("shows a comment box", () => {
-  const wrapper = shallow(<App />);
+let wrapper;
 
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   // test for the CommentBox component existence here
   expect(wrapper.find(CommentBox).length).toBe(1);
 });
 
 it("shows a comment list", () => {
-  const wrapper = shallow(<App />);
-
   // test for the CommentList component existence here
   expect(wrapper.find(CommentList).length).toBe(1);
 });
 
 /*
 toEqual vs toBe -> toBe implies strict equality, irrelevant for primitives, important for objects
+
+You can use toBe for primitives like strings, numbers or booleans for everything else use toEqual
+https://stackoverflow.com/questions/45195025/what-is-the-difference-between-tobe-and-toequal-in-jest?rq=1
+
 */
