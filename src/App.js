3d63aaf59985a9d48bca7c956a6277b1b30af0b0
stefaleon <stefaleon@gmail.com>
2019-12-07 22:22:42 +0200

    configure the redux store and the comments state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
+
 import CommentBox from "./components/CommentBox";
 import CommentList from "./components/CommentList";
 
+import store from "./store";
+
 function App() {
   return (
-    <div className="App">
-      <CommentBox />
-      <CommentList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CommentBox />
+        <CommentList />
+      </div>
+    </Provider>
   );
 }
 
