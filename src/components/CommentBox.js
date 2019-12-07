cc6c2f67a794db75f6b061741825c550881e0502
stefaleon <stefaleon@gmail.com>
2019-12-07 11:19:32 +0200

    cleanup and components

---
+++ src/components/CommentBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommentBox() {
+  return <div>Comment Box component</div>;
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommentList() {
+  return <div>Comment List component</div>;
+}
+
+export default CommentList;
