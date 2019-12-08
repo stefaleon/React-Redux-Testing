5aeefffcdcf01b5a668e83cc02c0a0021de0b81b
stefaleon <stefaleon@gmail.com>
2019-12-08 13:37:34 +0200

    move store.js into Root.js in order to fix initial state issues

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-import store from "./store";
+import reducers from "./reducers";
 
 const Root = ({ children, initialState }) => {
-  return <Provider store={{ ...store, initialState }}>{children}</Provider>;
+  return (
+    <Provider
+      store={createStore(
+        reducers,
+        initialState,
+        composeWithDevTools(applyMiddleware(thunk))
+      )}
+    >
+      {children}
+    </Provider>
+  );
 };
 
 export default Root;
